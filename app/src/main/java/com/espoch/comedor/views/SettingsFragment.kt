885f75bd5e683064c39c86655c7a5a8f64a2ad7f
jordy-ricardo25 <jordy.ricardo25@outlook.com>
2024-05-23 12:04:19 -0500

    Add `Settings` fragment

---
+++ app/src/main/java/com/espoch/comedor/views/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.espoch.comedor.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.espoch.comedor.databinding.FragmentHomeBinding
+import com.espoch.comedor.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
