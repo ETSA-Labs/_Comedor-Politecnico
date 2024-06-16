da5064e86006afc23f60827d37e893000b959e4c
jordy-ricardo25 <jordy.ricardo25@outlook.com>
2024-06-16 16:05:44 -0500

    fix 'FragmentBookingBinding' importation

--- app/src/main/java/com/espoch/comedor/views/BookingFragment.kt
+++ app/src/main/java/com/espoch/comedor/views/BookingFragment.kt
@@ -6,17 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.espoch.comedor.MainActivity
-import com.espoch.comedor.databinding.FragmentCartBinding
+import com.espoch.comedor.databinding.FragmentBookingBinding
 
 class BookingFragment : Fragment() {
-    private lateinit var binding: FragmentCartBinding
+    private lateinit var binding: FragmentBookingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCartBinding.inflate(inflater)
+        binding = FragmentBookingBinding.inflate(inflater)
         return binding.root
     }
 
