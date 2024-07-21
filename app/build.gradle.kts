9f1b3d34d40d406617e672c9e64fa1e28b498d1c
Melany <melany.orna@espoch.edu.ec>
2024-07-20 19:58:05 -0500

    Fix melany's branch merge issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.1"
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
